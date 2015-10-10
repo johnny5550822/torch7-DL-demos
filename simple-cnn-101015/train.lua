c46a7464cd71fbc547d3dcc6460dc6f914596db5
johnny5550822 <johnny5550822@gmail.com>
2015-10-10 15:45:12 -0700

    being simple CNN example

---
+++ simple-cnn-101015/train.lua
@@ -0,0 +1,52 @@
+-- ###############
+-- This is a minimal implementation of a CNN on digit (0-9) classification. This code is largely obtained from luacnn, https://github.com/hpenedones/luacnn
+
+-- The training data can be also obtained in https://github.com/hpenedones/luacnn
+-- ###############
+
+require "torch"
+require "nn"
+require "math"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
