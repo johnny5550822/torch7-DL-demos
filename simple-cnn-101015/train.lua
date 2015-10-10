37bf14a95a12307f02384e63a3942ba38dcaed03
johnny5550822 <johnny5550822@gmail.com>
2015-10-10 16:15:51 -0700

    Add data generation function

--- simple-cnn-101015/train.lua
+++ simple-cnn-101015/train.lua
@@ -16,20 +16,47 @@ maxIterations = 100000
 function create_network(nb_outputs)
 	local cnn = nn.Sequential();	
 
-    -- filter configuration
-    nInputPlane = 1; -- number of inputPlane
-    nOutputPlane = 10; --number of maps
-    filterSize = 5;
-    stride = 1;
-    padding = 0;
+	cnn:add(nn.SpatialConvolution(1,6,5,5)) -- becomes 12x12x6
+	cnn:add(nn.ReLU()) -- non-linear layer
+	cnn:add(nn.SpatioalSubSampling(6,2,2,2,2)) -- becomes 6x6x6
 
-    -- pooling configuration
-    
+	cnn:add(nn.Reshape(6*6*6))
+	cnn:add(nn.Linear(6*6*6,nb_outputs))
+	cnn:add(nn.LogSoftMax())
 
-	cnn:add(nn.SpatialConvolution(nInputPlane,nOutputPlane,filterSize,fitlerSize,stride,padding))
+	return cnn
+end
 
+--train a network
+function train_network(network,dataset)
+	print('Training the network......')
+	local criterion = nn.ClassNLLCriterion()
 
+	for iteration= 1, maxIterations do
+		local index = math.random(dataset:size()) --pick example at random
+		local input = dataset[index][1]
+		local output = dataset[index][2]
 
+		-- forward propagation
+		criterion:forward(network:forward(input),output)
+		-- zero the accumlated gradient
+		network:zeroGradParameters()
+		-- backward propagation
+		networkbackward(input,criterion:backward(network.output,output))
+		-- after backward propagation; update the parameters
+		network:updateParameters(learningRate)
+	end
+end
+
+--main 
+function main()
+	local training_dataset, testing_dataset, classes, classes_names = dofile('usps_dataset.lua')
+	local network = create_network(#classes)
+	train_network(network,training_dataset)
+end
+
+--run 
+main()
 
 
 
