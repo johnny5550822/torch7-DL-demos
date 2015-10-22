5a8783d35240eea72ab95cae8bf7d041c549844a
johnny5550822 <johnny5550822@gmail.com>
2015-10-21 23:31:11 -0700

    Add train_batch; with bugs

--- simple-cnn-101015/train_batch.lua
+++ simple-cnn-101015/train_batch.lua
@@ -1,8 +1,5 @@
 -- ###############
-
--- a version with GPU support. Assuming your gpu is working properly. Later I may release a version that check your gpu as well
-
--- This is a minimal implementation (e.g., no batch processing) of a CNN on digit (0-9) classification. This code is largely obtained from luacnn, https://github.com/hpenedones/luacnn
+-- This version is built on top of train.lua, which supports batch processing
 -- The training data can be also obtained in https://github.com/hpenedones/luacnn
 
 -- CPU only
@@ -11,8 +8,6 @@
 require "torch"
 require "nn"
 require "math"
-require "cutorch" -- for GPU, Cuda
-require "cunn" -- for GPU, Cuda
 
 -- add command line input
 cmd = torch.CmdLine()
@@ -30,10 +25,11 @@ params =cmd:parse(arg)
 -- variables
 learningRate = params.lr
 maxIterations = params.maxI
-
--- initiate gpu support
-cutorch.setDevice(1) -- gpu device # 1
-cutorch.manualSeed(123) -- seed 
+batch_size = 50	
+maxEpochs = 10
+totalImages = 10000 -- we know there are in total 10,000 images; each with size of 1 x 16 x 16
+patchSize = 16
+maxIterations = totalImages/batch_size -- this is per epochs; I did not calculate the max. total iterations for clarity
 
 -- create the neural network
 function create_network(nb_outputs)
@@ -53,38 +49,49 @@ function create_network(nb_outputs)
 	cnn:add(nn.Linear(40*3*3,nb_outputs))
 	cnn:add(nn.LogSoftMax())
 
-	return cnn:cuda()
+	return cnn
 end
 
 --train a network
 function train_network(network,dataset)
 	print('Training the network......')
-	local criterion = nn.ClassNLLCriterion():cuda()
+	local criterion = nn.ClassNLLCriterion()
 
-	for iteration= 1, maxIterations do
-		print(string.format('Iteration(max=%d) No.%d',maxIterations,iteration))
+	-- batch processing parameters
+    batch_counter = 1
 
-		local index = math.random(dataset:size()) --pick example at random
-		local input = dataset[index][1] -- size 1x16x16
-		local output = dataset[index][2] -- size 1
+	-- loop
+	for epoch = 1, maxEpochs do
+		for iteration= 1, maxIterations do
+			print(string.format('Epoch No:%d. Iteration(max=%d) No.%d',epoch,maxIterations,iteration))
 
-		-- DEBUG: To check the network output size
-		--print(network:forward(input))
-		--os.exit()
+			--[[
+			local index = math.random(dataset:size()) --pick example at random
+			local input = dataset[index][1] -- size 1x16x16
+			local output = dataset[index][2] -- size 1
+			]]
 
-		-- for cuda
-		input = input:float():cuda()
+			-- prepare for the batch
+			startPos = batch_size * (batch_counter-1) + 1
+			endPos = batch_size * batch_counter
+			input = torch.Tensor(batch_size,1,patchSize,patchSize):zero() -- this iteration batch
+			output = torch.Tensor(batch_size):zero()
+			k = 1 -- internal counter for the batch
+			for i=startPos,endPos do
+				input[{{k},1,{},{}}]:copy(dataset[i][1])
+				output[k] = dataset[i][2]
+				k = k + 1
+			end
 
-		output = torch.Tensor(1):fill(output):float():cuda()
-
-		-- forward propagation
-		criterion:forward(network:forward(input),output)
-		-- zero the accumlated gradient
-		network:zeroGradParameters()
-		-- backward propagation
-		network:backward(input,criterion:backward(network.output,output))
-		-- after backward propagation; update the parameters
-		network:updateParameters(learningRate)
+			-- forward propagation
+			criterion:forward(network:forward(input),output)
+			-- zero the accumlated gradient
+			network:zeroGradParameters()
+			-- backward propagation
+			network:backward(input,criterion:backward(network.output,output))
+			-- after backward propagation; update the parameters
+			network:updateParameters(learningRate)
+		end
 	end
 end
 
@@ -100,8 +107,6 @@ function test_predictor(predictor, test_dataset, classes, classes_names)
 
                local input  = test_dataset[i][1]
                local class_id = test_dataset[i][2]
-
-               input = input:float():cuda()
         
                local responses_per_class  =  predictor:forward(input) 
                local probabilites_per_class = torch.exp(responses_per_class)
@@ -122,20 +127,25 @@ function test_predictor(predictor, test_dataset, classes, classes_names)
 
 end
 
+-- shuffle the data
+function shuffleData(data)
+	local newOrder = torch.randperm(data:size(1))
+	local shuffle_data = {}
+	for i = 1,data:size(1) do
+		shuffle_data[i] = data[newOrder[i]]
+	end
+
+	return shuffle_data
+end
+
 --main 
 function main()
-	-- get the data
 	local training_dataset, testing_dataset, classes, classes_names = dofile('usps_dataset.lua')
 	local network = create_network(#classes)
+	s_training_dataset = shuffleData(training_dataset)
 
-	-- Timer
-	timer = torch.Timer()
-
-	train_network(network,training_dataset)
+	train_network(network,s_training_dataset)
 	test_predictor(network, testing_dataset, classes, classes_names)
-
-	-- end timer
-	print("Total time for training(s):%s",timer:time().real)
 end
 
 --run 
